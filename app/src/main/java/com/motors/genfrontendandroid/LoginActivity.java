f0f363c03db99617169b58f9a99939491918a88e
mlmrmail <mlmrmail@gmail.com>
2017-06-19 11:25:34 +0300

    second commit

--- app/src/main/java/com/motors/genfrontendandroid/LoginActivity.java
+++ app/src/main/java/com/motors/genfrontendandroid/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.motors.genfrontendandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Configuration config = getResources().getConfiguration();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            NormalLogin nl_fragment = new NormalLogin();
+            fragmentTransaction.replace(android.R.id.content, nl_fragment);
+        }else{
+            FingerprintLogin fl_fragment = new FingerprintLogin();
+            fragmentTransaction.replace(android.R.id.content, fl_fragment);
+        }
+        fragmentTransaction.commit();
+
+
+
     }
 }
